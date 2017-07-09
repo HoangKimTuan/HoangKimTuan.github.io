85e040fe29d34a0956181dfe34ba79981b76b0fa
HoangKimTuan <hoangkimtuan1996@gmail.com>
2017-07-09 20:24:42 +0700

    WebRTC - MediaStream APIs

--- client.js
+++ client.js
@@ -1,22 +1,54 @@
-//checks if the browser supports WebRTC 
+var stream;
+  
 function hasUserMedia() { 
-   navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia 
-      || navigator.mozGetUserMedia || navigator.msGetUserMedia; 
-   return !!navigator.getUserMedia; 
-}
+   //check if the browser supports the WebRTC 
+   return !!(navigator.getUserMedia || navigator.webkitGetUserMedia || 
+      navigator.mozGetUserMedia); 
+} 
  
-if (hasUserMedia()) { 
+if (hasUserMedia()) {
    navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia
-      || navigator.mozGetUserMedia || navigator.msGetUserMedia;
+      || navigator.mozGetUserMedia; 
 		
-   //get both video and audio streams from user's camera 
-   navigator.getUserMedia({ video: true, audio: true }, function (stream) { 
+   //enabling video and audio channels 
+   navigator.getUserMedia({ video: true, audio: true }, function (s) { 
+      stream = s; 
       var video = document.querySelector('video'); 
 		
-      //insert stream into the video tag 
+      //inserting our stream to the video tag     
       video.src = window.URL.createObjectURL(stream); 
    }, function (err) {}); 
 	
-}else {
-   alert("Error. WebRTC is not supported!"); 
+} else { 
+   alert("WebRTC is not supported"); 
 }
+  
+btnGetAudioTracks.addEventListener("click", function(){ 
+   console.log("getAudioTracks"); 
+   console.log(stream.getAudioTracks()); 
+});
+  
+btnGetTrackById.addEventListener("click", function(){ 
+   console.log("getTrackById"); 
+   console.log(stream.getTrackById(stream.getAudioTracks()[0].id)); 
+});
+  
+btnGetTracks.addEventListener("click", function(){ 
+   console.log("getTracks()"); 
+   console.log(stream.getTracks()); 
+});
+ 
+btnGetVideoTracks.addEventListener("click", function(){ 
+   console.log("getVideoTracks()"); 
+   console.log(stream.getVideoTracks()); 
+});
+
+btnRemoveAudioTrack.addEventListener("click", function(){ 
+   console.log("removeAudioTrack()"); 
+   stream.removeTrack(stream.getAudioTracks()[0]); 
+});
+  
+btnRemoveVideoTrack.addEventListener("click", function(){ 
+   console.log("removeVideoTrack()"); 
+   stream.removeTrack(stream.getVideoTracks()[0]); 
+});
